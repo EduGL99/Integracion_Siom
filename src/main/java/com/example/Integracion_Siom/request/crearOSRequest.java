0c5bafbc296548ea8b0b41545de26ecb97f3022e
EduGL99 <93292498+EduGL99@users.noreply.github.com>
2022-02-28 15:44:44 -0600

    Add files via upload

---
+++ src/main/java/com/example/Integracion_Siom/request/crearOSRequest.java
@@ -0,0 +1,22 @@
+package com.example.Integracion_Siom.request;
+
+public class crearOSRequest {
+
+    int cont_id;
+    int empl_id;
+    int empr_id;
+    int [] formularios;
+    int [] id_acompanante;
+    int id_despachador;
+    int id_jcuadrilla;
+    String descripcion;
+    String fecha_creacion;
+    String fecha_solicitud;
+    String indisponibilidad;
+    String tag;
+    String tipo;
+    int sube_id;
+    String user_login_creador;
+    String user_pass_creador;
+
+}

fb9c5d1a4ec42e051592fdb7a3869c4d54484a0b
AlexDev31 <92401341+AlexDev31@users.noreply.github.com>
2022-03-04 13:44:44 -0600

    Modelos y controllers
    Integra modelos y procedimientos para la modificación de un project y el envio de project

---
+++ src/main/java/com/example/Integracion_Siom/controller/ProjectController.java
@@ -0,0 +1,47 @@
+package com.example.Integracion_Siom.controller;
+
+import com.example.Integracion_Siom.Models.Project;
+import com.example.Integracion_Siom.Models.WorkOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectController {
+    List<WorkOrder> workOrders = new ArrayList<>();
+
+    public Project CrearProject(){
+        workOrders.add(new WorkOrder("123548798450","EN PROGRESO","123456789461"));
+        workOrders.add(new WorkOrder("321","EN PROGRESO","123456789461"));
+        Project project = new Project();
+        project.setId("12365478945");
+        project.setName("Prueba");
+        project.setDescription("Descripción de prueba");
+        project.setCreatedBy("Don creación");
+        project.setCreateTime("03-03-2022 10:12:31");
+        project.setType("78945612646");
+        project.setTemplate("45678513546");
+        project.setLocation("La presa");
+        project.setWorkOrders(workOrders);
+        project.setNumberOfWorkOrders("8");
+        project.setProperties("asdasd");
+        project.setComments("Comentarios");
+        return project;
+    }
+
+    public boolean EsExterno(Project project){
+        if(project.getType().equals("Externo"))
+            return true;
+        if(project.getWorkOrders().get(0).getState().equals("EN PROGRESO"))
+            return true;
+        return false;
+    }
+
+    public void SolicitaSIOM(Project project){
+        Project newProject = CrearProject();
+        if(EsExterno(newProject)){
+            //Aqui llamas al request
+        }else{
+            //nada
+        }
+    }
+}
